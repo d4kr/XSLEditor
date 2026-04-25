35d422a185578f4ae501a3afdb71225d2491ae7d
d4kr <d4kr@users.noreply.github.com>
2026-04-25 08:55:43 +0200

    feat(build): add Launcher shim, update manifest, harden -Pversion override
    - Add Launcher.java that calls Application.launch(XSLEditorApp.class, args)
      to bypass JavaFX bootstrap check when running java -jar fat JAR
    - Update application.mainClass, jar manifest, and shadowJar manifest to
      point to ch.ti.gagi.xsleditor.Launcher
    - Replace hardcoded version = '0.3.0' with findProperty guard so
      ./gradlew shadowJar -Pversion=X.Y.Z stamps version.properties correctly

---
+++ src/main/java/ch/ti/gagi/xsleditor/Launcher.java
@@ -0,0 +1,9 @@
+package ch.ti.gagi.xsleditor;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(XSLEditorApp.class, args);
+    }
+}
